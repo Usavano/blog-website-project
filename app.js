b4a64229fac5c047e1aa0a5a5b6c8f77bf266a76
Ivan Usatyi <usaivan2003@gmail.com>
2023-06-24 11:01:30 +0300

    Little changes in app.js

--- app.js
+++ app.js
@@ -57,21 +57,6 @@ app.post('/compose', (req,res) => {
     res.redirect('/');
 })
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
